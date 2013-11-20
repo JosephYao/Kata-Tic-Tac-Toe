bf8012363f345df35e4243473f63792555342d7a
josephyao <dermott@126.com>
2013-11-20 15:56:28 +0800

    back to initial code

--- src/Game.java
+++ src/Game.java
@@ -1,46 +1,48 @@
-
 public class Game {
-	private static final int ROW_LENGTH = 3;
-	private static final char AVAILABLE_POSITION = '-';
-	private static final char NO_WINNER = '-';
-	private final String board;
+	public StringBuffer board;
 
-	public Game(String board) {
-		this.board = board;
+	public Game(String s) {
+		board = new StringBuffer(s);
+	}
+
+	public Game(StringBuffer s, int position, char player) {
+		board = new StringBuffer();
+		board.append(s);
+		board.setCharAt(position, player);
 	}
 
 	public int move(char player) {
-		for (int index = possibleMove(0); isAPossibleMove(index); index = possibleMove(index + 1))
-			if (doesPlayerWin(player, index))
-				return index;
+		for (int i = 0; i < 9; i++) {
+			if (board.charAt(i) == '-') {
+				Game game = play(i, player);
+				if (game.winner() == player)
+					return i;
+			}
+		}
 
-		return possibleMove(0);
+		for (int i = 0; i < 9; i++) {
+			if (board.charAt(i) == '-')
+				return i;
+		}
+
+		return -1;
 	}
 
-	private boolean doesPlayerWin(char player, int possibleMove) {
-		StringBuffer newBoard = new StringBuffer(this.board);
-		newBoard.setCharAt(possibleMove, player);
-		return new Game(newBoard.toString()).winner() == player;
-	}
-
-	private boolean isAPossibleMove(int index) {
-		return index != -1;
-	}
-
-	private int possibleMove(int fromIndex) {
-		return board.indexOf(AVAILABLE_POSITION, fromIndex);
+	public Game play(int i, char player) {
+		return new Game(this.board, i, player);
 	}
 
 	public char winner() {
-		for (int position = 0; position < board.length(); position += ROW_LENGTH)
-			if (winnerAtPosition(position))
-				return board.charAt(position);
+		if (board.charAt(0) != '-' && board.charAt(0) == board.charAt(1)
+				&& board.charAt(1) == board.charAt(2))
+			return board.charAt(0);
+		if (board.charAt(3) != '-' && board.charAt(3) == board.charAt(4)
+				&& board.charAt(4) == board.charAt(5))
+			return board.charAt(3);
+		if (board.charAt(6) != '-' && board.charAt(6) == board.charAt(7)
+				&& board.charAt(7) == board.charAt(8))
+			return board.charAt(6);
 
-		return NO_WINNER;
-	}
-
-	private boolean winnerAtPosition(int position) {
-		return board.charAt(position) != AVAILABLE_POSITION && board.charAt(position) == board.charAt(position + 1)
-				&& board.charAt(position + 1) == board.charAt(position + 2);
+		return '-';
 	}
 }
